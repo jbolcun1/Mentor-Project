56c3b5a79ce6f24ec8294b5939c5fa785ecd8f30
Jakub Bolcun <jbolcun1@sheffield.ac.uk>
2021-03-14 14:13:41 +0000

    merging to pull

---
+++ spec/unit/app_spec.rb
@@ -0,0 +1,19 @@
+require "rspec"
+require "rack/test"
+
+require_relative "../spec_helper"
+
+RSpec.describe "App Test" do
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  describe "GET /" do
+    it "has a status code of 200 (OK)" do
+      get "/"
+      expect(last_response.status).to eq(200)
+    end
+  end
+end
