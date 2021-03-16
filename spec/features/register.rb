0883c12c01a4115a33e6a70673c7fb6e2abf7c64
Vlad Cranga <vmcranga1@sheffield.ac.uk>
2021-03-16 12:27:44 +0000

    Capybara FIX and HTML fix

--- spec/features/register.rb
+++ spec/features/register.rb
@@ -1,24 +1,16 @@
 require_relative "../spec_helper"
 
 describe "the register page" do
-    it "is accessible from the index page" do
+  it "is accessible from the index page" do
     visit "/"
     click_button "register"
-    save_page
-    expect(last_response.status).to eq(200)
+    expect(page.status_code ).to eq(200)
   end
 
   it "can display fields" do
     visit "/register"
-    save_page
-    expect(page).to include "Email:"
-    expect(page).to include "Password:"
+    expect(page).to have_content "Email:"
+    expect(page).to have_content "Password:"
   end
 
-  it "display an error message when fields are empty" do
-    visit "/register"
-    save_page
-    click_button "Sign Up"
-    expect(page).to have_content "Please fill out this field."
-  end
 end
