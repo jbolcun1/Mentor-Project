abce0331b252599d22be242997e96d08ba96082c
Vlad Cranga <vmcranga1@sheffield.ac.uk>
2021-03-26 11:43:26 +0000

    rspec fix

--- spec/features/profile_spec.rb
+++ spec/features/profile_spec.rb
@@ -37,17 +37,17 @@ describe "the profile page" do
     expect(page).to have_content "Please fill in the fields you wish to change:"
   end
     
-  it "displays an error message when the old password is not entered" do
+  it "displays the dashboard if the old password is not entered" do
     visit "/login"
     fill_in "email", with: "Mentor1@gmail.com"
     fill_in "password", with: "Password1"
     click_button "Submit"
     click_link "Profile"
     click_button "Change Details"
-    expect(page).to have_content "Enter old password to change it to the new one"
+    expect(page).to have_content "Dashboard"
   end
     
-  it "displays an error message when the new passwords don't match" do
+  it "displays an error message if the new passwords don't match" do
     visit "/login"
     fill_in "email", with: "Mentor1@gmail.com"
     fill_in "password", with: "Password1"
