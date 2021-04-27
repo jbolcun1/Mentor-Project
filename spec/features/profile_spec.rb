64e718b4649f2a84ea5555142bbb174db581eb24
Vlad Cranga <vmcranga1@sheffield.ac.uk>
2021-04-27 06:35:45 +0000

    profile testing

--- spec/features/profile_spec.rb
+++ spec/features/profile_spec.rb
@@ -18,23 +18,52 @@ describe "the profile page" do
     click_link "Profile"
     expect(page.status_code).to eq(200)
   end
+    
+  it "is accessible by logged in admins from the dashboard" do
+    visit "/login"
+    fill_in "email", with: "ahaque3@sheffield.ac.uk"
+    fill_in "password", with: "admin"
+    click_button "Submit"
+    click_link "Profile"
+    expect(page.status_code).to eq(200)
+  end
 
-  it "displays information about the page to a logged in mentee" do
+  it "displays profile information to a logged in mentee" do
     visit "/login"
     fill_in "email", with: "Mentee1@gmail.ac.uk"
     fill_in "password", with: "Password1"
     click_button "Submit"
     click_link "Profile"
-    expect(page).to have_content "Please fill in the fields you wish to change:"
+    expect(page).to have_content "University:"
+    expect(page).to have_content "Degree:"
+    expect(page).to have_content "Telephone:"
+    expect(page).to have_content "Description Of Yourself:"  
   end
 
-  it "displays information about the page to a logged in mentor" do
+  it "displays profile information to a logged in mentor" do
     visit "/login"
     fill_in "email", with: "Mentor1@gmail.com"
     fill_in "password", with: "Password1"
     click_button "Submit"
     click_link "Profile"
+    expect(page).to have_content "Title:"
+    expect(page).to have_content "Job Title:"
+    expect(page).to have_content "Industry Sector:"
+    expect(page).to have_content "Available Time:"
+    expect(page).to have_content "Description Of Yourself:"
+  end
+    
+  it "displays profile information to a logged in admin" do
+    visit "/login"
+    fill_in "email", with: "ahaque3@sheffield.ac.uk"
+    fill_in "password", with: "admin"
+    click_button "Submit"
+    click_link "Profile"
     expect(page).to have_content "Please fill in the fields you wish to change:"
+    expect(page).to have_content "First Name:"
+    expect(page).to have_content "Last Name:"
+    expect(page).to have_content "Email:"
+    expect(page).to have_content "Description Of Yourself:"
   end
 
   it "displays the dashboard if the old password is not entered" do
