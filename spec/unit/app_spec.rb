478bf87457706f6f9bc80c8882bdf3f488bffebd
Edmond Chu <eyjchu1@sheffield.ac.uk>
2021-05-02 13:17:56 +0000

    Unit testing for routes

--- spec/unit/app_spec.rb
+++ spec/unit/app_spec.rb
@@ -9,11 +9,92 @@ RSpec.describe "App Test" do
   def app
     Sinatra::Application
   end
-
+  
+  describe "GET /index" do
+    it "has status code of 200 (OK)" do
+      get "/index"
+      expect(last_response.status).to eq(200)
+    end
+    
+    it "says 'Helping you to find the perfect mentor Quickly. Effiently.'" do
+      get "/index"
+      expect(last_response.body).to include "Helping you to find the perfect mentor"
+    end
+  end
+  
+  describe "GET /about" do
+    it "has a status code of 200 (OK)" do
+      get "/about"
+      expect(last_response.status).to eq(200)
+    end
+    
+    it "says 'How the E-mentor scheme works:'" do
+      get "/about"
+      expect(last_response.body).to include "How the E-mentor scheme works:"
+    end
+  end
+  
+  describe "GET /register" do
+    it "has a status code of 200 (OK)" do
+      get "/register"
+      expect(last_response.status).to eq(200)
+    end
+    
+    it "says 'Please fill in the fields below to complete the registration process'" do
+      get "/register"
+      expect(last_response.body).to include "Please fill in the fields below to complete the registration process"
+    end
+  end
+  
   describe "GET /" do
+    context "When no user is logged in" do
+      it "has a status code of 302 (Redirect)" do
+        get "/"
+        expect(last_response.status).to eq(302)
+      end
+    end 
+  end
+  
+  describe "GET /logout" do
     it "has a status code of 302 (Redirect)" do
-      get "/"
+      get "/logout"
       expect(last_response.status).to eq(302)
     end
+  end
+  
+  describe "GET /dashboard" do
+    context "When no user is logged in" do
+      it "has a status code of 302 (Redirect)" do
+        get "/dashboard"
+        expect(last_response.status).to eq(302)
+      end
+    end 
+  end
+  
+  #doesn't work
+  describe "GET /profile" do
+    context "When no user is logged in" do
+      it "has a status code of 302 (Redirect)" do
+        get "/profile"
+        expect(last_response.status).to eq(302)
+      end
+    end 
+  end
+  
+  #doesn't work
+  describe "GET /make-report" do
+    context "When no user is logged in" do
+      it "has a status code of 302 (Redirect)" do
+        get "/make-report"
+        expect(last_response.status).to eq(302)
+      end
+    end 
+  end
+  
+  describe "GET /gibberish" do
+    it "has a status code of 404 (Not Found)" do
+      get "/gibberish"
+      expect(last_response.status).to eq(404)
+    end 
   end
 end
