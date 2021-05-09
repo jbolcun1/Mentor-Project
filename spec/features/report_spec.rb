c8b369c3bcc3778d3cd2a7b08a5e0a5fbf4c043a
Vlad Cranga <vmcranga1@sheffield.ac.uk>
2021-05-09 17:41:54 +0000

    report spec

---
+++ spec/features/report_spec.rb
@@ -0,0 +1,77 @@
+require_relative "../spec_helper"
+
+describe "the report functionality" do
+  it "is accessible by logged in mentees" do
+    mentee_login
+    visit "/make-report"
+    expect(page.status_code).to eq(200)
+  end
+
+  it "can display relevant content to a logged in mentee" do
+    mentee_login
+    visit "make-report"
+    expect(page).to have_content "Make a report"
+    expect(page).to have_content "Please enter the details of the report you want to make"
+    expect(page).to have_content "Description of what has happened:"
+    expect(page).to have_button "Submit Report"
+  end
+
+  it "allows the mentee to submit a report" do
+    mentee_login
+    visit "/make-report"
+    fill_in "identifier", with: "Mentor2@gmail.com"
+    fill_in "description", with: "bad mentor xd"
+    click_button "Submit"
+    expect(page).to have_content "Mentee Dashboard"
+  end
+    
+  it "is accessible by logged in mentors" do
+    mentor_login
+    visit "/make-report"
+    expect(page.status_code).to eq(200)
+  end
+
+  it "can display relevant content to a logged in mentor" do
+    mentor_login
+    visit "make-report"
+    expect(page).to have_content "Make a report"
+    expect(page).to have_content "Please enter the details of the report you want to make"
+    expect(page).to have_content "Description of what has happened:"
+    expect(page).to have_button "Submit Report"
+  end
+
+  it "allows the mentor to submit a report" do
+    mentor_login
+    visit "/make-report"
+    fill_in "identifier", with: "Mentor1@gmail.com"
+    fill_in "description", with: "bad mentor lol"
+    click_button "Submit"
+    expect(page).to have_content "Mentor Dashboard"
+  end 
+    
+  it "is accessible by logged in admins" do
+    admin_login
+    visit "/view-reports"
+    expect(page.status_code).to eq(200)
+  end
+
+  it "can display relevant content to a logged in admin" do
+    admin_login
+    visit "/view-reports"
+    expect(page).to have_content "Reports Below"
+    expect(page).to have_content "Name/ Email of offender"
+    expect(page).to have_content "View More"
+  end
+
+  it "allows the admin to view and delete a report" do
+    admin_login
+    visit "/view-report-detail?id=1"
+    expect(page).to have_content "The ID of the report creator 1"
+    expect(page).to have_content "The person the report is against Mentor2@gmail.com"
+    expect(page).to have_content "bad mentor xd"
+    expect(page).to have_content "Delete report?"
+    click_link "Delete report?"
+    expect(page).to have_content "Reports Below"
+  end     
+    
+end
